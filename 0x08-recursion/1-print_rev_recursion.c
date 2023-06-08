2aa3b1b050a38a5fef2811e4f001fc464e528e16
Peter <ceopeter2003@gmail.com>
2023-06-07 18:06:21 -0700

    ok

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - good
+ * @s: good
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+
+	{
+
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar (*s);
+
+
+
+
+
+}
