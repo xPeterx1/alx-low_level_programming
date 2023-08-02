93636e80c671949241378cdd17dc596d41ee60be
Peter <ceopeter2003@gmail.com>
2023-08-02 10:55:15 -0700

    Modification on task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
- /**
-  * _strlen_recursion - count the letters
-  * @s: pointer to the string
-  * Return:the number of letters
-  */
+/**
+ * _strlen_recursion - to count the letters
+ * @s: the pointer to the string
+ * Return: number of l
+ */
 
 int _strlen_recursion(char *s)
 {
-	if(*s == 0)
+	if (*s == 0)
 	{
-	return 0;
+	return (0);
 	}
 	return (1 + _strlen_recursion(s + 1));
 
