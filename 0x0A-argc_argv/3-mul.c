fec32560b3f2e20e9011ca21838bc8a64d2d7339
Peter <ceopeter2003@gmail.com>
2023-08-06 12:32:05 -0700

    t3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print p name
+ * @argc: arg count
+ * @argv : aray of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc == 3)
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+	printf("Error\n");
+	return (0);
+}
