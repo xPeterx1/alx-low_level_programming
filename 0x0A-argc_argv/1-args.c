5c30a3c46d4f9fe1235f931f0f79da50c0f4a4aa
Peter <ceopeter2003@gmail.com>
2023-08-06 12:08:15 -0700

    task 1 argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print p name
+ * @argc: arg count
+ * @argv : aray of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
