e41ae685ed476f7359f28df59b002fc44673cccd
Peter <ceopeter2003@gmail.com>
2023-08-08 19:12:19 -0700

    t2 modification

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,13 +10,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *d;
-	int lenght;
-	int i;
-	int j;
-	int k;
+	int i, lenght, j, k;
 
 	if (s1 == NULL && s2 == NULL)
-	return (NULL);
+	{	s1 = "";
+		s2 = "";
+	}
 	else if (s1 == NULL && s2 != NULL)
 	s1 = "";
 	else if (s2 == NULL)
