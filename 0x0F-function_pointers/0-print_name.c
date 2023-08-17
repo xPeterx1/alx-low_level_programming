e9ff4ef72853e6c03ba5eb32bd7e4455492c90d4
Peter <ceopeter2003@gmail.com>
2023-08-16 18:21:56 -0700

    Hello

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_name - h
+ * @name: hy
+ * @f : f
+ * RETURN: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+
+
+
+
+
+}
