26b34c0a85685d2f62b5e96943f10270260010af
Peter <ceopeter2003@gmail.com>
2023-10-22 03:01:26 -0700

    My doggy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,28 +1,52 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+/**
+ * new_dog - Function that creates a new dog.
+ * @name: type pointer char name.
+ * @age: type pointer float age
+ * @owner: type pointer char owner
+ * Return: 0.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
-	dog_t newDog;
-	dog_t *pointer = &newDog;
+	int numofw, word;
+	dog_t *pointer = malloc(sizeof(dog_t));
+
 	if (pointer != NULL)
+{
+	for (numofw = 0, word = 0; *(name + word) != '\0'; word++, numofw++)
+	;
+	pointer->name = malloc(sizeof(char) * numofw + 1);
+		if (pointer->name == NULL)
+		{
+			free(pointer);
+			return (NULL);
+		}
+	for (word = 0; word <= (numofw + 1); word++)
 	{
-	newDog.name = name;
-	newDog.age = age;
-	newDog.owner = owner;
-	char *namesaver = name;
-	char *ownersaver = owner;
-	return (pointer);
+		*(pointer->name + word) = *(name + word);
+
 	}
+	for (numofw = 0, word = 0; *(owner + word) != '\0'; word++, numofw++)
+	;
+	pointer->owner = malloc(sizeof(char) * numofw + 1);
+		if (pointer->owner == NULL)
+		{
+			free(pointer);
+			return (NULL);
+		}
+
+	for (word = 0; word <= (numofw + 1); word++)
+	{
+		*(pointer->owner + word) = *(owner + word);
+
+	}
+	pointer->age = age;
+	return (pointer);
+}
 	return (NULL);
-
-
-
-
-
-
-
 
 }
 
