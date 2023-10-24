ed0c79c893ee776a181e87cda6b49de432e8db65
Peter <ceopeter2003@gmail.com>
2023-10-24 14:37:04 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - good
+ * @n: good
+ * Return: good
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i, sum = 0;
+
+va_list args;
+va_start(args, n);
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
+va_end(args);
+return (sum);
+
+
+
+
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
