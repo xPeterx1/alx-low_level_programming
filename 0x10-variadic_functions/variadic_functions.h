302855fc6a97edcca480ca71bf54e3ce3cf999c9
Peter <ceopeter2003@gmail.com>
2023-10-24 14:48:21 -0700

    fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,4 @@
 #pragma once
-#include <stdarg.h>
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
