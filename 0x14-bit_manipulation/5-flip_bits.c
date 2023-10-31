db7c7c1e6b2cb8506bdd8418988a1bb7789af972
Peter <ceopeter2003@gmail.com>
2023-10-31 09:42:46 -0700

    Finally I can play with these goddamn bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,23 +1,29 @@
 #include "main.h"
-
 /**
  * flip_bits - Returns the number of bits  needed to be flipped
  * @n: type unsigned long int input pointer
  * @m: type unsigned long int position of unit
  * Return: flipped_bit
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int or;
-	unsigned int flip_bit;
+	unsigned long int numberofbytes, temp = 0;
 
-	or = n ^ m;
-	flip_bit = 0;
-	while (or > 0)
-	{
-		if ((or & 1) != 0)
-			flip_bit++;
-		or = or >> 1;
-	}
-	return (flip_bit);
+
+	temp = n ^ m;
+
+	if (temp & 1)
+	numberofbytes++;
+	if (temp == 0)
+	return (numberofbytes);
+	temp >>= 1;
+
+
+
+
+
+
+
+
 }
