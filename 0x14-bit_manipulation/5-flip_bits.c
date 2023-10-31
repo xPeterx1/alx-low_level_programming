3cbd2bddd6821a79a3f84b85a9eb170a95e48b33
Peter <ceopeter2003@gmail.com>
2023-10-31 11:36:39 -0700

    Finally I nailed it

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,30 +1,25 @@
 #include "main.h"
+
 /**
  * flip_bits - Returns the number of bits  needed to be flipped
  * @n: type unsigned long int input pointer
  * @m: type unsigned long int position of unit
  * Return: flipped_bit
  */
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int numberofbytes, temp = 0;
+	unsigned long int numberofbytes = 0, temp = 0;
 
 
 	temp = n ^ m;
 
 	while (temp > 0)
 	{
-	if ((temp & 1) != 0)
+	if (temp & 1)
 	numberofbytes++;
-	temp =temp >> 1;
+	temp >>= 1;
 	}
 
 	return (numberofbytes);
-
-
-
-
-
 
 }
