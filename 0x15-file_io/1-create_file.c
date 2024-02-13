b07699e781ad203c360a785d94a1f950996c5d9c
root <root@LAPTOP-KOLD1Q7N>
2024-02-13 18:36:13 +0200

    YALA

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * create_file - Function that creates a file
  * @filename: type char filename pointer of FD
@@ -8,24 +7,35 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int final_out = 0;
-	int  file = 0;
-	int  x = 0;
+int fd, wr, count, temp;
 
-	if (filename == NULL)
-		return (-1);
-	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
-		return (-1);
-	if (text_content != NULL)
-	{
-		while (*(text_content + x) != '\0')
-		{
-			x++;
-		}
-		final_out = write(file, text_content, x);
-	}
-	if (final_out == -1 && final_out != x)
-		return (-1);
+if (filename == NULL)
+{
+	return (-1);
+}
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (fd == -1)
+{
+	return (-1);
+}
+
+if (text_content == NULL)
+{
 	return (1);
+}
+else
+{
+	for (count = 0; *(text_content + count) != '\0'; count++)
+	{
+	temp++;
+	}
+	wr = write(fd, text_content, sizeof(char) * (count + 1));
+	if (wr == -1)
+	{
+		return (-1);
+	}
+}
+
+	return (1);
+
 }
