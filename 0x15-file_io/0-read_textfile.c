0cc998f37b8cd55e51d91d6e533304f51678c50c
root <root@LAPTOP-KOLD1Q7N>
2024-02-13 16:55:51 +0200

    fix ana

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: type char filename
+ * @letters: type size_t letters
+ * Return: if cannot be oppened or read 0, if is NULL 0, if writes fail 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	if (filename == NULL)
