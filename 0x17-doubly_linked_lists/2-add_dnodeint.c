c70f6ea87d0bf95a5fd05b5ce712f800480ad6de
PETER <ceopeter2003@gmail.com>
2024-03-21 23:05:15 +0200

    FEN ELMOSHKELA

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * add_dnodeint - m
+ * @head: h
+ * @n: s
+ * Return: 1
+ **/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+
+	newnode = malloc(sizeof(dlistint_t));
+	newnode->n  = n;
+	newnode->prev = NULL;
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		newnode->next = NULL;
+		*head = newnode;
+
+	}
+
+	else
+	{
+		newnode->next = *(head);
+		(*head)->prev = newnode;
+		*(head) = newnode;
+
+	}
+
+
+}
