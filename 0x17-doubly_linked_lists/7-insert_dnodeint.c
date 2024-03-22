2e3ef43200985062fb5a00c3151d33029e7041d8
PETER <ceopeter2003@gmail.com>
2024-03-22 03:38:48 +0200

    AFTH ELTAREK

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - f
+ * @h:3
+ * @idx: 4
+ * @n: 5
+ * Return: 9
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp, *newnode;
+	unsigned int numofnodes;
+
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	tmp = *h;
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
+	}
+	numofnodes = 0;
+	while (tmp != NULL)
+	{
+		if (numofnodes == idx)
+		{
+			newnode->next = tmp;
+			(tmp->prev)->next = newnode;
+			newnode->prev = tmp->prev;
+			tmp->prev = newnode;
+			return (newnode);
+		}
+		tmp = tmp->next;
+		numofnodes++;
+	}
+	if (idx == numofnodes + 1)
+	{
+		add_dnodeint_end(h, n);
+	}
+
+	return (NULL);
+}
