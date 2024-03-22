dc4d285c70bb5c1b3419041d9338ee95b8a5ad7c
PETER <ceopeter2003@gmail.com>
2024-03-22 04:26:43 +0200

    HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp = *h;
 	if (idx == 0)
 	{
-		add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 	}
 	numofnodes = 0;
 	while (tmp != NULL)
@@ -42,7 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 		if (numofnodes == idx)
 		{
-			add_dnodeint_end(h, n);
+			return (add_dnodeint_end(h, n));
 		}
 	return (NULL);
 }
