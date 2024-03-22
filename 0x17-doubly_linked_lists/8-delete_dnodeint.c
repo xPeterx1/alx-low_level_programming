9e63f1f8a8240b742386d815f9b1ba3042243e7b
PETER <ceopeter2003@gmail.com>
2024-03-22 04:52:19 +0200

    HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - f
+ * @head:3
+ * @index: 4
+ * Return: 9
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int numofnodes;
+
+	if (head == NULL || (*head) == NULL)
+	{
+		return (-1);
+	}
+	tmp = *head;
+	if (index == 0)
+	{
+		*head = tmp->next;
+		tmp->next->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+	numofnodes = 0;
+	while (tmp != NULL)
+	{
+		if (numofnodes == index)
+		{
+			tmp->prev->next = tmp->next;
+			tmp->next->prev = tmp->prev;
+			free(tmp);
+			return (1);
+		}
+		tmp = tmp->next;
+		numofnodes++;
+	}
+	return (-1);
+}
