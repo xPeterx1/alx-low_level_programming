566f79076f5b3a489983e3b82917e6edaf98bfb1
root <root@LAPTOP-KOLD1Q7N>
2024-04-12 01:39:38 +0200

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - ok
+ * @size: done
+ * Return: ok
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tmp;
+
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+
+	tmp = malloc(sizeof(hash_table_t));
+	if (!tmp)
+	{
+		return (NULL);
+	}
+	tmp->array = calloc(size, sizeof(hash_node_t));
+	if (!(tmp->array))
+	{
+		return (NULL);
+	}
+	tmp->size = size;
+	return (tmp);
+}
