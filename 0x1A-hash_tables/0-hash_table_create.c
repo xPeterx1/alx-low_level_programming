ea302b8f49185fdd5de725ff4d4853869a2c4953
root <root@LAPTOP-KOLD1Q7N>
2024-04-12 01:59:18 +0200

    HA

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,21 +7,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *tmp;
+	unsigned long i;
 
 	if (size <= 0)
 	{
 		return (NULL);
 	}
 	tmp = malloc(sizeof(hash_table_t));
-	if (tmp == NULL)
+	if (!tmp)
 	{
 		return (NULL);
 	}
 	tmp->array = calloc(size, sizeof(hash_node_t *));
-	if (tmp->array == NULL)
+	if (!(tmp->array))
 	{
 		return (NULL);
 	}
 	tmp->size = size;
+	for (i = 0; i < tmp->size; i++)
+		tmp->array[i] = NULL;
 	return (tmp);
 }
