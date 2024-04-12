f3e2ead26e1bff6b68f23aa882e2179d5c83ce78
root <root@LAPTOP-KOLD1Q7N>
2024-04-12 20:35:54 +0200

    ya sba7 elfol

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,48 +7,59 @@
  **/
 void print_list(hash_node_t *h)
 {
-	while (h)
+	int i;
+	int j;
+
+	i = 0;
+	j = -1;
+	while (h != NULL)
 	{
+		j++;
+		if (j >= 1)
+		{
+		printf(", ");
+		}
 		printf("'%s': '%s'", h->key, h->value);
-		if (h->next)
-			printf(", ");
 		h = h->next;
+		i++;
 	}
 }
 
 /**
- * hash_table_print - prints a hash table
- * @ht: hash table to print
- **/
+ * hash_table_print - good
+ * @ht: good
+ * Return: 11
+*/
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *current;
 	unsigned long int i;
-	hash_node_t *node = NULL;
-	char *last_key = NULL;
-	unsigned long int index;
+	int j;
 
-	if (!ht)
-		return;
-
+	j = -1;
+	if (ht == NULL)
+	return;
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
-			node = ht->array[i];
-	}
 
-	printf("{");
-
-	if (node)
-	{
-		last_key = node->key;
-		index = key_index((const unsigned char *)last_key, ht->size);
-		for (i = 0; i < ht->size; i++)
+		if (ht->array[i])
 		{
-			print_list(ht->array[i]);
-			if (ht->array[i] && i < index)
-				printf(", ");
+			j++;
+			current = ht->array[i];
+			if (j >= 1)
+			{
+			printf(", ");
+			}
+			if (current->next == NULL)
+			{
+				printf("'%s': '%s'", current->key, current->value);
+			}
+			else
+			{
+				print_list(current);
+			}
 		}
 	}
-
 	printf("}\n");
 }
