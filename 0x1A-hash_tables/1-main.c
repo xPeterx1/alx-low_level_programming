9d534470b51333b37e062346455b044b5a63c58f
root <root@LAPTOP-KOLD1Q7N>
2024-04-12 02:33:05 +0200

    ss

---
+++ 0x1A-hash_tables/1-main.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+int main(void)
+{
+    char *s;
+
+    s = "cisfun";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "Don't forget to tweet today";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "98";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    return (EXIT_SUCCESS);
+}
